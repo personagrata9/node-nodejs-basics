d97124a8447b7b9fc70a5a24b66a66948788cc49
Iryna Bakhur <ira.bakhur@icloud.com>
2022-06-02 00:14:45 +0300

    feat: implement print file content function

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,3 +1,20 @@
+import * as path from 'path';
+import * as url from 'url';
+import { readFile } from 'fs/promises';
+
 export const read = async () => {
-    // Write your code here 
+  const __dirname = url.fileURLToPath(new URL('.', import.meta.url));
+  const folderName = 'files';
+  const fileToReadName = 'fileToRead.txt';
+  const fileToReadPath =  path.join(__dirname, folderName, fileToReadName);
+
+  const errorMessage = 'FS operation failed';
+
+  await readFile(fileToReadPath, { encoding: 'utf-8' })
+    .then((content) => console.log(content))
+    .catch(() => {
+      throw new Error(errorMessage);
+    })
 };
+
+read();
