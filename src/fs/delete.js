4e55b685781cdda353c8efe3aeb5c29a8184521f
Iryna Bakhur <ira.bakhur@icloud.com>
2022-06-01 23:06:37 +0300

    feat: implement delete file function

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,3 +1,19 @@
+import * as path from 'path';
+import * as url from 'url';
+import { unlink } from 'fs/promises';
+
 export const remove = async () => {
-    // Write your code here 
+  const __dirname = url.fileURLToPath(new URL('.', import.meta.url));
+  const folderName = 'files';
+  const fileToRemoveName = 'fileToRemove.txt';
+  const fileToRemovePath = path.join(__dirname, folderName, fileToRemoveName);
+  
+  const errorMessage = 'FS operation failed';
+
+  await unlink(fileToRemovePath)
+    .catch(() => {
+      throw new Error(errorMessage);
+    })
 };
+
+remove();
