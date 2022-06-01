4a72a65c1963f810b1cd16302ef4553a9abba88b
Iryna Bakhur <ira.bakhur@icloud.com>
2022-06-01 22:48:49 +0300

    feat: implement rename wrong file name function

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,3 +1,33 @@
+import * as path from 'path';
+import * as url from 'url';
+import * as fs from 'fs/promises';
+
 export const rename = async () => {
-    // Write your code here 
+  const __dirname = url.fileURLToPath(new URL('.', import.meta.url));
+  const folderName = 'files';
+  const wrongFileName = 'wrongFilename.txt';
+  const properFileName = 'properFilename.md';
+
+  const oldPath = path.join(__dirname, folderName, wrongFileName);
+  const newPath = path.join(__dirname, folderName, properFileName);
+
+  const errorMessage = 'FS operation failed';
+
+  let wrongFileIsExist = false;
+
+  await fs.access(newPath)
+    .then(() => {
+      wrongFileIsExist = true;
+    })
+    .catch(async () => {
+      await fs.rename(oldPath, newPath)
+        .catch(() => {
+          throw new Error(errorMessage);
+        });
+    })
+    .finally(() => {
+      if (wrongFileIsExist) throw new Error(errorMessage);
+    })
 };
+
+rename();
