408d3051083609fcd6ec54ff31e562b1d1f03ac5
Iryna Bakhur <ira.bakhur@icloud.com>
2022-06-01 20:21:35 +0300

    feat: implement create new file function

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,3 +1,22 @@
+import * as path from 'path';
+import * as url from 'url';
+import { writeFile } from 'fs/promises';
+
 export const create = async () => {
-    // Write your code here 
+  const __dirname = url.fileURLToPath(new URL('.', import.meta.url));
+  const folderName = 'files';
+  
+  const fileName = 'fresh.txt';
+  const fileContent = 'I am fresh and young';
+  const filePath = path.join(__dirname, folderName, fileName);
+
+  const errorMessage = 'FS operation failed';
+
+writeFile(filePath, fileContent, {flag: 'ax'})
+  .then(() => console.log(`${fileName} was created successfully in ${folderName} folder!`))
+  .catch(() => {
+    throw new Error(errorMessage);
+  });
 };
+
+create();
