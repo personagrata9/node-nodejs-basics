50dafdc5d1fe087ad761f30e225e8bdbbe32f02c
Iryna Bakhur <ira.bakhur@icloud.com>
2022-06-02 20:19:14 +0300

    refactor: change parsing env vars function

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,7 +1,8 @@
 export const parseEnv = () => {
    const RSSVariables = Object.entries(process.env).filter((entry) => entry[0].startsWith('RSS_'));
-   const result = RSSVariables.map((variable) => `${variable[0]}=${variable[1]}`).join('; ');
-   console.log(result);
+   const resultArr = RSSVariables.map((variable) => `${variable[0]}=${variable[1]}`);
+   
+   if (resultArr.length) console.log(resultArr.join('; '));
 };
 
 parseEnv();
