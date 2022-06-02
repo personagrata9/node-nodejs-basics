48dfaa10734cd2e315ca45f0e98f5ad9ac327ce6
Iryna Bakhur <ira.bakhur@icloud.com>
2022-06-02 20:21:07 +0300

    feat: implement parsing command line arguments function

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,3 +1,10 @@
 export const parseArgs = () => {
-    // Write your code here 
+  const args = process.argv.slice(2);
+  const props = args.filter((_, index) => index % 2 === 0);
+  const values = args.filter((_, index) => index % 2 !== 0);
+  const resultArr = props.map((prop, index) => `${prop.slice(2)} is ${values[index]}`);
+
+  console.log(resultArr.join(', '));
 };
+
+parseArgs();
