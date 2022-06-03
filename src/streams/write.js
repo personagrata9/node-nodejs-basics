10f295b645d2b1a7e2f1876569e8d3d1fe798103
Iryna Bakhur <ira.bakhur@icloud.com>
2022-06-04 00:06:03 +0300

    feat: implement write stream

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,3 +1,20 @@
+import * as path from 'path';
+import { fileURLToPath } from 'url';
+import { createWriteStream } from 'fs';
+
 export const write = async () => {
-    // Write your code here 
+  const __dirname = fileURLToPath(new URL('.', import.meta.url));
+  const folderName = 'files';
+  const fileToWriteName = 'fileToWrite.txt';
+  const fileToWritePath = path.join(__dirname, folderName, fileToWriteName);
+
+  console.log(`Enter text to write it into file ${fileToWriteName} or press ctrl + c to exit`);
+
+  process.stdin.on('data', (chunk) => {
+    const writableToFileStream = createWriteStream(fileToWritePath, { flags: 'a'});
+    writableToFileStream.write(chunk.toString());
+    writableToFileStream.close();
+  })
 };
+
+write();
