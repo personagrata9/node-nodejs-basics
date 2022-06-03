ec268c5ed14b2185f67e12ad93984831bd02893b
Iryna Bakhur <ira.bakhur@icloud.com>
2022-06-03 18:54:31 +0300

    feat: implement calculating hash

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,3 +1,26 @@
-export const calculateHash = async () => {
-    // Write your code here 
+import * as path from 'path';
+import { fileURLToPath } from 'url';
+import { readFile } from 'fs/promises';
+
+const { createHash } = await import('crypto');
+
+const getDataFromFile = async (fileName) => {
+  const __dirname = fileURLToPath(new URL('.', import.meta.url));
+  const folderName = 'files';
+  const filePath = path.join(__dirname, folderName, fileName);
+
+  return await readFile(filePath, { encoding: 'utf-8' });
 };
+
+export const calculateHash = async () => {
+  const fileName = 'fileToCalculateHashFor.txt';
+  
+  const sha256Hasher = createHash('sha256');
+  const data = await getDataFromFile(fileName);
+  const hash = sha256Hasher.update(data).digest('hex');
+
+  console.log(`Hash for file ${fileName} is ${hash}`);
+  return hash;
+};
+
+calculateHash();
