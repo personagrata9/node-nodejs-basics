69d488164e7e1f700369d248b3cb3fe8045769ec
Iryna Bakhur <ira.bakhur@icloud.com>
2022-06-05 00:21:38 +0300

    feat: implement spawn child process function

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,3 +1,20 @@
+import * as path from 'path';
+import { fileURLToPath } from 'url';
+import { fork } from 'child_process';
+
 export const spawnChildProcess = async (args) => {
-    // Write your code here
+  const __dirname = fileURLToPath(new URL('.', import.meta.url));
+  const childPath = path.join(__dirname, 'files', 'script.js');
+  
+  const child = fork(childPath, args, { stdio: ['pipe', 'pipe', 'pipe', 'ipc']});
+
+  process.stdin.pipe(child.stdin);
+
+  child.stdout.on('data', (data) => {
+    process.stdout.write(`Received from child process:\n${data}\n`);
+  });
 };
+
+const args = process.argv.slice(2);
+
+spawnChildProcess(args);
