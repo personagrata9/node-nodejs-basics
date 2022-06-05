7e22745ee25007222b572ee5120e5565f162396d
Iryna Bakhur <ira.bakhur@icloud.com>
2022-06-05 13:07:36 +0300

    refactor: change local server message at start

--- src/modules/test.mjs
+++ src/modules/test.mjs
@@ -6,6 +6,6 @@ console.log(unknownObject);
 const myServer = createMyServer;
 const PORT = 8000;
 myServer.listen(PORT, () => {
-  console.log(`Local server is running on port ${PORT}`);
+  console.log(`Local server is started at port :${PORT}`);
   console.log('Press ctrl + c to exit');
 });
